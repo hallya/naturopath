dec1c525b0216f051c0ec732f20c5e935cdc5819
Lucien Dulac <lucien.dulac@healthhero.com>
2023-04-10 21:48:55 +0200

    styles: configure tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -1,19 +1,18 @@
 import type { MetaFunction } from "@vercel/remix";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
 import { Analytics } from "@vercel/analytics/react";
+import stylesheet from "~/tailwind.css";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "Léa Dulac - Naturopathe",
   viewport: "width=device-width,initial-scale=1",
 });
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: stylesheet }];
+};
 
 export default function App() {
   return (
--- remix.config.js
+++ remix.config.js
@@ -2,7 +2,10 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-  ignoredRouteFiles: ['**/.*'],
+  ignoredRouteFiles: ["**/.*"],
+  future: {
+    unstable_tailwind: true,
+  },
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
