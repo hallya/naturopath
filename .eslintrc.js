525d832d7f2b8bfb25fca220ce2238d86abe8a01
Lucien Dulac <lucien.dulac@healthhero.com>
2023-04-10 23:56:21 +0200

    chore: update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,15 @@
 module.exports = {
   extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node"],
+  rules: {
+    "sort-imports": [
+      "error",
+      {
+        ignoreCase: false,
+        ignoreDeclarationSort: false,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ["none", "all", "multiple", "single"],
+        allowSeparatedGroups: false,
+      },
+    ],
+  },
 };
