568391d47e864f3caefb4ef946e6be60510ce0e1
Lucien Dulac <lucien.dulac@healthhero.com>
2024-11-18 00:37:58 +0100

    chore: remove dead code

--- app/routes/parcours/route.tsx
+++
@@ -1,9 +0,0 @@
-import { useLocation } from "@remix-run/react";
-import { PageHeader } from "../../views";
-import { mapNavLinks } from "../navLinks";
-
-export default function Route() {
-  const { pathname } = useLocation();
-
-  return <PageHeader title={mapNavLinks[pathname].pageTitle} />;
-}
